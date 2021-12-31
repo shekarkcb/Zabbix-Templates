19d5be29d39748ad97d4917205638605f5738446
Shekar <shekarkcb@gmail.com>
2021-12-31 19:37:24 +0530

    Create dhcpAllocated.bat

---
+++ dhcpAllocated.bat
@@ -0,0 +1,6 @@
+# Main script which would try to pull Allocated IP's on the DHCP server.
+# You can write another wrapper above this to run from poweshell.
+# This would output in json, which then parsed for exact IP Address.
+# @shekarkcb
+
+netsh dhcp server show mibinfo | select-string -pattern $args[0] | -Context 0,3 | findstr use | %{$data = $_.split("="); Write-Output "$($date[1])"} | %{$str = $.trim("."); Write-Output "$str?}
