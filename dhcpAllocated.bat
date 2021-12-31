c778812bd45631a2762e77f5c99e54f792840123
Shekar <shekarkcb@gmail.com>
2021-12-31 20:05:29 +0530

    Update dhcpAllocated.bat

--- dhcpAllocated.bat
+++ dhcpAllocated.bat
@@ -1,6 +1,6 @@
-# Wrapper for dhcpAllocated.ps1
-# You need to pass the scope as argument to this script.
-# Returns allocated IP from dhcpAllocated.ps1
-# @shekarkcb
+Rem Wrapper for dhcpAllocated.ps1
+Rem You need to pass the scope as argument to this script.
+Rem Returns allocated IP from dhcpAllocated.ps1
+Rem @shekarkcb
 @echo off
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\zabbix\scripts\dhcpAllocated.ps1" %1
